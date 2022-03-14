42ed87534b86c1d5b80435b33673fd1e2a4d339d
Bob-Loth <bob94loth@gmail.com>
2022-03-13 22:56:12 -0700

    declination angle should be working now.

--- gerryStats.h
+++ gerryStats.h
@@ -9,25 +9,27 @@
 
 using namespace std;
 
+double RadToDeg(double rad) { return (180.0 / M_PI) * rad; }
+
+double calcXWeight(size_t numDistricts) { return (numDistricts / 2) + .5; }
+
 double getDeclinationAngle(vector<double> demVoteShares) {
     vector<double> above50;
     vector<double> below50;
 
     sort(demVoteShares.begin(), demVoteShares.end());
     for (auto pct : demVoteShares) {
-        (pct > 0.5) ? above50.push_back(pct) : below50.push_back(pct);
+        (pct > 0.5) ? above50.push_back(pct - 0.5)
+                    : below50.push_back(0.5 - pct);
     }
-    // compute the scaling factor for the best-fit lines
-    double aboveRatio =
-        static_cast<double>(demVoteShares.size()) / above50.size();
-    double belowRatio =
-        static_cast<double>(demVoteShares.size()) / below50.size();
+    // compute the X scaling factor for the best-fit lines
+    double aboveMidPoint = calcXWeight(above50.size());
+    double belowMidPoint = calcXWeight(below50.size());
 
-    // calculate the angles of the best fit lines
-    double theta =
-        atan2(0.5 - stats::computeMean(below50), (below50.size() / 2.0));
-    double gamma =
-        atan2((stats::computeMean(above50) - 0.5), (above50.size() / 2.0));
+    // calculate the angles from center of line to average of each half of
+    // points
+    double theta = atan2(stats::computeMean(below50), belowMidPoint);
+    double gamma = atan2(stats::computeMean(above50), aboveMidPoint);
 
     // debug
     cout << "average dem win: " << stats::computeMean(above50) << endl;
@@ -37,6 +39,6 @@ double getDeclinationAngle(vector<double> demVoteShares) {
 
     // calculate the declination angle as 2/π * best fit angle of above -
     // best fit angle of below
-    double declinationAngle = (M_2_PI) * (gamma - theta);
-    return declinationAngle;
+    double declinationAngle = M_2_PI * (gamma - theta);
+    return RadToDeg(declinationAngle);
 }
--- main.cpp
+++ main.cpp
@@ -10,6 +10,21 @@ using namespace std;
 
 int main() {
     vector<double> washington = {.4033, .4442, .4722, .5094, .5678,
-                                 .5728, .5928, .6246, .7124, .8604};
+                                 .5728, .5928, .6246, .7124, .9604};
+    cout << "\ntesting with washington's 10 districts" << endl;
     cout << getDeclinationAngle(washington) << endl;
+
+    vector<double> testOddNum = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
+                                 0.6, 0.7, 0.8, 0.9, 1.0};
+    cout << "\ntesting with 11 values" << endl;
+    cout << getDeclinationAngle(testOddNum) << endl;
+
+    vector<double> testEvenNum1 = {0.0,  0.1, 0.2, 0.3, 0.4, 0.45,
+                                   0.55, 0.6, 0.7, 0.8, 0.9, 1.0};
+    cout << "\ntesting with 12 values" << endl;
+    cout << getDeclinationAngle(testEvenNum1) << endl;
+
+    vector<double> testEvenNum2 = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0};
+    cout << "\ntesting with 6 values" << endl;
+    cout << getDeclinationAngle(testEvenNum2) << endl;
 }
